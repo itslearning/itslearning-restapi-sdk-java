cea5ce2990379edab231cf03ad9acb5a34c08ea5
amundoz <amundoz@513eb65a-5214-11de-a3da-17e148e7a168>
2009-06-08 10:08:52 +0000

    first checkin

---
+++ trunk/RestApi.Sdk.Common/src/itslearning/platform/restApi/sdk/common/CryptographyHelper.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package itslearning.platform.restApi.sdk.common;
+
+/**
+ *
+ * @author amund
+ */
+public class CryptographyHelper {
+
+}
