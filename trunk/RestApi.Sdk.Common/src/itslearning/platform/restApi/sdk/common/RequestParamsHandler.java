334e8a4f2b0343374961340539dd0fe0c81db47a
amundoz <amundoz@513eb65a-5214-11de-a3da-17e148e7a168>
2009-08-26 21:36:22 +0000

    porting, this does not compile yet, temp commit

--- trunk/RestApi.Sdk.Common/src/itslearning/platform/restApi/sdk/common/RequestParamsHandler.java
+++ trunk/RestApi.Sdk.Common/src/itslearning/platform/restApi/sdk/common/RequestParamsHandler.java
@@ -20,4 +20,39 @@ public class RequestParamsHandler<TRequestParams extends IRequestParams> {
             return reqParams.getPageUrl();
         }
         // TODO: it seems that this reflection bit might be hard in java
+
+        /// <summary>
+        /// Gets the parameters from the query string.
+        /// </summary>
+        /// <param name="queryStringParams"></param>
+        /// <returns></returns>
+        public TRequestParams getParams(NameValueCollection queryStringParams)
+        {
+            var requestParams = Activator.CreateInstance<TRequestParams>();
+
+            foreach (var property in GetProperties())
+            {
+                string value = queryStringParams[property.Name];
+                if (!string.IsNullOrEmpty(value))
+                {
+                    Type propertyType = property.PropertyType;
+
+                    // Nullable<T> => T
+                    if (propertyType.IsGenericType && propertyType.GetGenericTypeDefinition() == typeof (Nullable<>))
+                    {
+                        propertyType = propertyType.GetGenericArguments()[0];
+                    }
+
+                    bool castSuccessed;
+                    object castedValue = CastToPrimitiveType(value, propertyType, out castSuccessed);
+
+                    // do not set value if convertion failed
+                    if (castSuccessed)
+                    {
+                        property.SetValue(requestParams, castedValue, null);
+                    }
+                }
+            }
+            return requestParams;
+        }
 }
