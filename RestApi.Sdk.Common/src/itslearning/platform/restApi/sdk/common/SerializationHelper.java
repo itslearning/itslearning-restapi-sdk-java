e7d1de5f6738afb0646e4fdd8243068499e0d6d1
amundoz <amundoz@513eb65a-5214-11de-a3da-17e148e7a168>
2009-08-28 14:29:20 +0000

    had to leave, checkin for later work at home

--- RestApi.Sdk.Common/src/itslearning/platform/restApi/sdk/common/SerializationHelper.java
+++ RestApi.Sdk.Common/src/itslearning/platform/restApi/sdk/common/SerializationHelper.java
@@ -5,10 +5,33 @@
 
 package itslearning.platform.restApi.sdk.common;
 
+import itslearning.platform.restApi.sdk.common.entities.Constants.LearningObjectInstancePermissions;
+
 /**
  * Common serialization logic.
  * @author Amund Trovåg <amund@itslearning.com>
  */
 public class SerializationHelper {
+    /**
+     * Serializes LearningObjectInstancePermissions flags as a string.
+     * @param permissions
+     * @return Serialized permissions
+     */
+        public static String learningObjectInstancePermissionsToString(LearningObjectInstancePermissions permissions)
+        {
+            // The same code is in ItslUserInfo.ToString method
 
+            StringBuilder sb = new StringBuilder();
+            
+            if ((permissions & LearningObjectInstancePermissions.READ) != 0)
+                sb.append("R");
+            if ((permissions & LearningObjectInstancePermissions.PARTICIPATE) != 0)
+                sb.append("P");
+            if ((permissions & LearningObjectInstancePermissions.EVALUATE) != 0)
+                sb.append("E");
+            if ((permissions & LearningObjectInstancePermissions.MODIFY) != 0)
+                sb.append("M");
+
+            return sb.toString();
+        }
 }
