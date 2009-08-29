9eb7f2be0cac774d59c89589a12d5e70fca7efa0
amundoz <amundoz@513eb65a-5214-11de-a3da-17e148e7a168>
2009-08-29 21:24:19 +0000

    Authorization header added to request. Now I get a classcast excpetion from webservice call

--- trunk/RestApi.Sdk.LearningToolApp/src/itslearning/platform/restapi/sdk/learningtoolapp/LearningObjectServiceClientProxy.java
+++ trunk/RestApi.Sdk.LearningToolApp/src/itslearning/platform/restapi/sdk/learningtoolapp/LearningObjectServiceClientProxy.java
@@ -4,30 +4,39 @@
  */
 package itslearning.platform.restapi.sdk.learningtoolapp;
 
+import itslearning.platform.restApi.sdk.common.entities.ApiSession;
 import javax.xml.ws.BindingProvider;
+import javax.xml.ws.handler.MessageContext;
 import org.tempuri.*;
+
 /**
  * LearningObjectService client wrapper.
  * @author Amund Trovåg
  */
 class LearningObjectServiceClientProxy implements ILearningObjectService
 {
+
+    private org.tempuri.LearningObjectService _service = new org.tempuri.LearningObjectService();
+    private String _endPointAddress = "";
+    private ApiSession _apiSession;
+
     public GetLearningObjectInstanceResponse getLearningObjectInstance(GetLearningObjectInstance parameters)
     {
         try
         { // Call Web Service Operation
-            org.tempuri.LearningObjectService service = new org.tempuri.LearningObjectService();
-            org.tempuri.ILearningObjectService port = service.getWebHttpBindingILearningObjectService();
-            
-            BindingProvider bp = (BindingProvider) port;
-            bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "http://restapi.itslearning.com/");
+            org.tempuri.ILearningObjectService port = _service.getWebHttpBindingILearningObjectService();
 
+            BindingProvider bp = (BindingProvider) port;
+            // Set correct endpoint address.  This is imporant since the service will be available in different
+            // environments.
+            bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, _endPointAddress);
+            String authHeader = AuthorizationHelper.toAuthorizationHeader(_apiSession);
+            bp.getRequestContext().put(MessageContext.HTTP_REQUEST_HEADERS, authHeader);
             // TODO process result here
             org.tempuri.GetLearningObjectInstanceResponse result = port.getLearningObjectInstance(parameters);
-            System.out.println("Result = "+result);
+            System.out.println("Result = " + result);
             return result;
-        }
-        catch (Exception ex)
+        } catch (Exception ex)
         {
             throw new RuntimeException(ex);
         }
@@ -51,7 +60,26 @@ class LearningObjectServiceClientProxy implements ILearningObjectService
 
     public GetPossibleAssessmentStatusesResponse getPossibleAssessmentStatuses(GetPossibleAssessmentStatuses parameters)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try
+        { // Call Web Service Operation
+            org.tempuri.ILearningObjectService port = _service.getWebHttpBindingILearningObjectService();
+
+            BindingProvider bp = (BindingProvider) port;
+            // Set correct endpoint address.  This is imporant since the service will be available in different
+            // environments.
+            bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, _endPointAddress);
+            String authHeader = AuthorizationHelper.toAuthorizationHeader(_apiSession);
+            bp.getRequestContext().put(MessageContext.HTTP_REQUEST_HEADERS, authHeader);
+            // TODO initialize WS operation arguments here
+            // TODO process result here
+            org.tempuri.GetPossibleAssessmentStatusesResponse result = port.getPossibleAssessmentStatuses(parameters);
+            System.out.println("Result = "+result);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            throw new RuntimeException(ex);
+        }
     }
 
     public GetAssessmentStatusItemsResponse getAssessmentStatusItems(GetAssessmentStatusItems parameters)
@@ -77,5 +105,37 @@ class LearningObjectServiceClientProxy implements ILearningObjectService
     public GetLearningObjectInstanceUserReportsResponse getLearningObjectInstanceUserReports(GetLearningObjectInstanceUserReports parameters)
     {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /**
+     * @return the _endPointAddress
+     */
+    public String getEndPointAddress()
+    {
+        return _endPointAddress;
+    }
+
+    /**
+     * @param endPointAddress the _endPointAddress to set
+     */
+    public void setEndPointAddress(String endPointAddress)
+    {
+        this._endPointAddress = endPointAddress;
+    }
+
+    /**
+     * @return the _apiSession
+     */
+    public ApiSession getApiSession()
+    {
+        return _apiSession;
+    }
+
+    /**
+     * @param apiSession the _apiSession to set
+     */
+    public void setApiSession(ApiSession apiSession)
+    {
+        this._apiSession = apiSession;
     }
 }
