2d1f0a5e48c23bd506cfbdf0d9a4296d376f5af5
amundoz <amundoz@513eb65a-5214-11de-a3da-17e148e7a168>
2009-09-04 19:41:45 +0000

    Works to deserialize xml to LearningObjectInstance. Ugly conversion...

--- trunk/RestApi.Sdk.LearningToolApp/src/itslearning/platform/restapi/sdk/learningtoolapp/LearningObjectServiceClientRest.java
+++ trunk/RestApi.Sdk.LearningToolApp/src/itslearning/platform/restapi/sdk/learningtoolapp/LearningObjectServiceClientRest.java
@@ -4,18 +4,20 @@
  */
 package itslearning.platform.restapi.sdk.learningtoolapp;
 
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.converters.basic.DateConverter;
-import com.thoughtworks.xstream.converters.basic.NullConverter;
-import com.thoughtworks.xstream.converters.extended.ISO8601DateConverter;
 import itslearning.platform.restApi.sdk.common.CryptographyHelper;
+import itslearning.platform.restApi.sdk.common.ExceptionTranslator;
+import itslearning.platform.restApi.sdk.common.HttpStatusWrapper;
 import itslearning.platform.restApi.sdk.common.entities.ApiSession;
 import itslearning.platform.restapi.sdk.learningtoolapp.entities.LearningObjectInstance;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.httpclient.HttpClient;
@@ -29,6 +31,16 @@ import org.apache.commons.httpclient.methods.PutMethod;
 import org.apache.commons.httpclient.params.DefaultHttpParams;
 import org.apache.commons.httpclient.params.HttpClientParams;
 import org.apache.commons.httpclient.params.HttpParams;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.Namespace;
+import org.dom4j.Node;
+import org.dom4j.QName;
+import org.dom4j.io.SAXReader;
+import org.jaxen.JaxenException;
+import org.jaxen.XPath;
+import org.jaxen.dom4j.Dom4jXPath;
 
 /**
  *
@@ -37,6 +49,13 @@ import org.apache.commons.httpclient.params.HttpParams;
 public class LearningObjectServiceClientRest implements ILearningObjectServiceRestClient
 {
 
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+
+    static
+    {
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
     private ApiSession _apiSession;
     private String _baseUri;
 
@@ -57,6 +76,105 @@ public class LearningObjectServiceClientRest implements ILearningObjectServiceRe
         GET, PUT, POST, DELETE;
     }
 
+    private LearningObjectInstance deserializeXMLToLearningObjectInstance(InputStream xmlStream) throws ParseException
+    {
+        LearningObjectInstance loi = null;
+        try
+        {
+            SAXReader reader = new SAXReader();
+            Document doc = reader.read(xmlStream);
+            String lElem = "//loi:LearningObjectInstance";
+
+            doc.getRootElement().setQName(new QName(doc.getRootElement().getQName().getName(),
+                    new Namespace("loi", doc.getRootElement().getNamespaceURI())));
+            Element root = doc.getRootElement();
+            if (root.getName().equals("LearningObjectInstance"))
+            {
+                loi = new LearningObjectInstance();
+
+                Node node = root.selectSingleNode(lElem + "/loi:ActiveToUtc");
+                if (node.hasContent())
+                {
+                    loi.setActiveToUTC(sdf.parse(node.getStringValue()));
+                }
+                node = root.selectSingleNode(lElem + "/loi:DeadlineUtc");
+                if (node.hasContent())
+                {
+                    loi.setDeadLineUTC(sdf.parse(node.getStringValue()));
+                }
+                node = root.selectSingleNode(lElem + "/loi:ActiveFromUtc");
+                if (node.hasContent())
+                {
+                    loi.setActiveFromUTC(sdf.parse(node.getStringValue()));
+                }
+                node = root.selectSingleNode(lElem + "/loi:CreatedUtc");
+                if (node.hasContent())
+                {
+                    loi.setCreatedUTC(sdf.parse(node.getStringValue()));
+                }
+                node = root.selectSingleNode(lElem + "/loi:ModifiedUtc");
+                if (node.hasContent())
+                {
+                    loi.setModifiedUTC(sdf.parse(node.getStringValue()));
+                }
+                node = root.selectSingleNode(lElem + "/loi:Title");
+                if (node.hasContent())
+                {
+                    loi.setTitle(node.getStringValue());
+                }
+                node = root.selectSingleNode(lElem + "/loi:LearningObjectInstanceId");
+                if (node.hasContent())
+                {
+                    loi.setLearningObjectInstanceId(Integer.parseInt(node.getStringValue()));
+                }
+                node = root.selectSingleNode(lElem + "/loi:LearningObjectId");
+                if (node.hasContent())
+                {
+                    loi.setLearningObjectId(Integer.parseInt(node.getStringValue()));
+                }
+                node = root.selectSingleNode(lElem + "/loi:ModifiedUtc");
+                if (node.hasContent())
+                {
+                    loi.setModifiedUTC(sdf.parse(node.getStringValue()));
+                }
+                node = root.selectSingleNode(lElem + "/loi:CreatedUtc");
+                if (node.hasContent())
+                {
+                    loi.setCreatedUTC(sdf.parse(node.getStringValue()));
+                }
+                node = root.selectSingleNode(lElem + "/loi:CreatedByUserId");
+                if (node.hasContent())
+                {
+                    loi.setCreatedByUserId(Integer.parseInt(node.getStringValue()));
+                }
+                node = root.selectSingleNode(lElem + "/loi:IsObligatory");
+                if (node.hasContent())
+                {
+                    loi.setIsObligatory(Boolean.parseBoolean(node.getStringValue()));
+                }
+                node = root.selectSingleNode(lElem + "/loi:AssessmentId");
+                if (node.hasContent())
+                {
+                    loi.setAssessmentId(Integer.parseInt(node.getStringValue()));
+                }
+                node = root.selectSingleNode(lElem + "/loi:AssessmentStatusId");
+                if (node.hasContent())
+                {
+                    loi.setAssessmentStatusId(Integer.parseInt(node.getStringValue()));
+                }
+
+
+
+            }
+        } catch (DocumentException ex)
+        {
+            // TODO: throw exception
+            Logger.getLogger(LearningObjectServiceClientRest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return loi;
+    }
+
     protected HttpMethod getInitializedHttpMethod(HttpClient client, String uri, HttpMethodType methodType)
     {
         Calendar now = GregorianCalendar.getInstance();
@@ -69,7 +187,7 @@ public class LearningObjectServiceClientRest implements ILearningObjectServiceRe
         switch (methodType)
         {
             case GET:
-                 method = new GetMethod(uri);
+                method = new GetMethod(uri);
                 break;
             case PUT:
                 method = new PutMethod(uri);
@@ -90,31 +208,35 @@ public class LearningObjectServiceClientRest implements ILearningObjectServiceRe
         return method;
     }
 
-
     public LearningObjectInstance getLearningObjectInstance(int instanceId, int learningObjectId)
     {
 
-        String uri = String.format(_baseUri+"/Restapi/LearningObjectService.svc/learningObjects/%s/instances/%s", learningObjectId, instanceId);
+        String uri = String.format(_baseUri + "/Restapi/LearningObjectService.svc/learningObjects/%s/instances/%s", learningObjectId, instanceId);
         HttpClient httpClient = new HttpClient();
         HttpMethod method = getInitializedHttpMethod(httpClient, uri, HttpMethodType.GET);
-
+        LearningObjectInstance loi = null;
         try
         {
             int statusCode = httpClient.executeMethod(method);
             if (statusCode != HttpStatus.SC_OK)
             {
-                // TODO, throw ItslException
-                throw new HttpException("TODO");
+                throw new RuntimeException(ExceptionTranslator.fromHttpStatus(new HttpStatusWrapper(statusCode)));
+            } else
+            {
+                loi = deserializeXMLToLearningObjectInstance(method.getResponseBodyAsStream());
             }
 
-            
+
         } catch (IOException ex)
+        {
+            throw new RuntimeException(ex);
+        } catch (ParseException ex)
         {
             throw new RuntimeException(ex);
         } finally
         {
             method.releaseConnection();
         }
-        return null;
+        return loi;
     }
 }
--- trunk/RestApi.Sdk.LearningToolApp/test/itslearning/platform/restapi/sdk/learningtoolapp/XStreamDateConverterTest.java
+++ trunk/RestApi.Sdk.LearningToolApp/test/itslearning/platform/restapi/sdk/learningtoolapp/XStreamDateConverterTest.java
@@ -4,40 +4,18 @@
  */
 package itslearning.platform.restapi.sdk.learningtoolapp;
 
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.converters.Converter;
-import com.thoughtworks.xstream.converters.ConverterLookup;
-import com.thoughtworks.xstream.converters.ConverterRegistry;
-import com.thoughtworks.xstream.converters.MarshallingContext;
-import com.thoughtworks.xstream.converters.UnmarshallingContext;
-import com.thoughtworks.xstream.converters.basic.DateConverter;
-import com.thoughtworks.xstream.converters.extended.ISO8601DateConverter;
-import com.thoughtworks.xstream.converters.extended.ISO8601GregorianCalendarConverter;
-import com.thoughtworks.xstream.converters.reflection.PureJavaReflectionProvider;
-import com.thoughtworks.xstream.converters.reflection.ReflectionProviderWrapper;
-import com.thoughtworks.xstream.core.DefaultConverterLookup;
-import com.thoughtworks.xstream.io.HierarchicalStreamReader;
-import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
-import com.thoughtworks.xstream.io.xml.DomDriver;
-import com.thoughtworks.xstream.mapper.CachingMapper;
-import com.thoughtworks.xstream.mapper.Mapper;
-import java.io.InputStream;
+import itslearning.platform.restapi.sdk.learningtoolapp.entities.LearningObjectInstance;
 import java.io.StringReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.TimeZone;
-import javax.xml.parsers.DocumentBuilderFactory;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.dom4j.Namespace;
 import org.dom4j.Node;
 import org.dom4j.QName;
-import org.dom4j.XPath;
-import org.dom4j.dom.DOMNamespace;
 import org.dom4j.io.SAXReader;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -52,9 +30,6 @@ import static org.junit.Assert.*;
  */
 public class XStreamDateConverterTest
 {
-
-    final ConverterLookup lookup = new DefaultConverterLookup();
-    ConverterRegistry registry;
 
     public XStreamDateConverterTest()
     {
@@ -81,26 +56,7 @@ public class XStreamDateConverterTest
     }
 
     @Test
-    public void testConvertDateXMLToObject()
-    {
-        String xmlDate = "<LearningObjectInstance><ActiveToUtc>9999-12-31T23:59:59Z</ActiveToUtc></LearningObjectInstance>";
-        String[] dateFormats = new String[]
-        {
-            "yyyyMMdd", "yyyy-MM-dd'T'HH:mm:ss'Z'", "yyyy-MM-dd"
-        };
-        XStream xStream = new XStream(new DomDriver());
-        xStream.alias("LearningObjectInstance", MyTestClass.class);
-
-        DateConverter converter = new DateConverter("yyyy-MM-dd'T'HH:mm:ss.S'Z'", dateFormats);
-        xStream.registerConverter(converter);
-
-        MyTestClass instance = (MyTestClass) xStream.fromXML(xmlDate);
-        assertNotNull(instance);
-
-    }
-
-    @Test
-    public void testConvertDateXMLToObjectWithDom4j() throws DocumentException, ParseException
+    public void testConvertLearningObjectInstanceXMLToObjectWithDom4j() throws DocumentException, ParseException
     {
         String xmlDate = "<LearningObjectInstance " +
                 "xmlns=\"http://schemas.datacontract.org/2004/07/Itslearning.Platform.RestApi.Sdk.LearningToolApp.Entities\" " +
@@ -111,34 +67,32 @@ public class XStreamDateConverterTest
         SAXReader reader = new SAXReader();
 
         Document doc = reader.read(new StringReader(xmlDate));
-        //doc.getRootElement().add(DocumentHelper.createNamespace("test", "http://schemas.datacontract.org/2004/07/Itslearning.Platform.RestApi.Sdk.LearningToolApp.Entities"));
         doc.getRootElement().setQName(new QName(doc.getRootElement().getQName().getName(),
                 new Namespace("loi", "http://schemas.datacontract.org/2004/07/Itslearning.Platform.RestApi.Sdk.LearningToolApp.Entities")));
-        MyTestClass tc = parseDocumentToTestClass(doc);
-
+        LearningObjectInstance tc = parseDocumentToTestClass(doc);
+        assertNotNull(tc.getActiveToUTC());
     }
 
-    private MyTestClass parseDocumentToTestClass(Document doc) throws ParseException
+    private LearningObjectInstance parseDocumentToTestClass(Document doc) throws ParseException
     {
-        treeWalk(doc);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-        MyTestClass myTestClass = null;
+        LearningObjectInstance myTestClass = null;
         Element root = doc.getRootElement();
 
         if (root.getName().equals("LearningObjectInstance"))
         {
-            myTestClass = new MyTestClass();
+            myTestClass = new LearningObjectInstance();
             Node node = root.selectSingleNode("//loi:LearningObjectInstance/loi:ActiveToUtc");
 
             if (node.hasContent())
             {
-                myTestClass.setActiveToUtc(sdf.parse(node.getStringValue()));
+                myTestClass.setActiveToUTC(sdf.parse(node.getStringValue()));
             }
             node = root.selectSingleNode("//loi:LearningObjectInstance/loi:CreatedUtc");
             if (node.hasContent())
             {
-                myTestClass.setCreatedUtc(sdf.parse(node.getStringValue()));
+                myTestClass.setCreatedUTC(sdf.parse(node.getStringValue()));
             }
             node = root.selectSingleNode("//loi:LearningObjectInstance/loi:Title");
             if (node.hasContent())
@@ -147,111 +101,5 @@ public class XStreamDateConverterTest
             }
         }
         return myTestClass;
-    }
-
-    public void treeWalk(Document document)
-    {
-        treeWalk(document.getRootElement());
-    }
-
-    public void treeWalk(Element element)
-    {
-        for (int i = 0, size = element.nodeCount(); i < size; i++)
-        {
-            Node node = element.node(i);
-            if (node instanceof Element)
-            {
-                treeWalk((Element) node);
-            }
-            else if (node instanceof Namespace)
-            {
-
-                // do something....
-            }
-        }
-    }
-
-    private void setUpRegistry()
-    {
-        registry = new ConverterRegistry()
-        {
-
-            public void registerConverter(final Converter converter, int priority)
-            {
-                ((ConverterRegistry) lookup).registerConverter(new Converter()
-                {
-
-                    public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context)
-                    {
-                        return reader.getAttribute("i:nil") != null ? null : converter.unmarshal(reader, context);
-                    }
-
-                    public void marshal(Object arg0, HierarchicalStreamWriter arg1, MarshallingContext arg2)
-                    {
-                    }
-
-                    public boolean canConvert(Class arg0)
-                    {
-                        return true;
-                    }
-                }, 0);
-            }
-        };
-    }
-
-    public class MyTestClass
-    {
-
-        private Date ActiveToUtc;
-        private Date CreatedUtc;
-        private String title;
-
-        /**
-         * @return the activeToUtc
-         */
-        public Date getActiveToUtc()
-        {
-            return ActiveToUtc;
-        }
-
-        /**
-         * @param activeToUtc the activeToUtc to set
-         */
-        public void setActiveToUtc(Date activeToUtc)
-        {
-            this.ActiveToUtc = activeToUtc;
-        }
-
-        /**
-         * @return the CreatedUtc
-         */
-        public Date getCreatedUtc()
-        {
-            return CreatedUtc;
-        }
-
-        /**
-         * @param CreatedUtc the CreatedUtc to set
-         */
-        public void setCreatedUtc(Date CreatedUtc)
-        {
-            this.CreatedUtc = CreatedUtc;
-        }
-
-        /**
-         * @return the title
-         */
-        public String getTitle()
-        {
-            return title;
-        }
-
-        /**
-         * @param title the title to set
-         */
-        public void setTitle(String title)
-        {
-            this.title = title;
-        }
     }
 }
