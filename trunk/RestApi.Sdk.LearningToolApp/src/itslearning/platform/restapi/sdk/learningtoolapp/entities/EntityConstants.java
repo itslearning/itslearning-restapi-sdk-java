ba8682b8478993001d9943b263a325a2056e1b18
amundoz <amundoz@513eb65a-5214-11de-a3da-17e148e7a168>
2009-09-11 13:30:06 +0000

    Constants for namespace in xml

---
+++ trunk/RestApi.Sdk.LearningToolApp/src/itslearning/platform/restapi/sdk/learningtoolapp/entities/EntityConstants.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package itslearning.platform.restapi.sdk.learningtoolapp.entities;
+
+/**
+ *
+ * @author Amund Trovåg <amund@itslearning.com>
+ */
+public class EntityConstants {
+
+    public static final String NAMESPACE_ENTITIES = "http://schemas.datacontract.org/2004/07/Itslearning.Platform.RestApi.Sdk.LearningToolApp.Entities";
+}
