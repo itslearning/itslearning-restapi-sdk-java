bb77a53b41f6554b94dc63196697692912a43484
amundoz <amundoz@513eb65a-5214-11de-a3da-17e148e7a168>
2009-09-14 09:00:49 +0000

    comment

--- MyFirstLearningTool/src/java/itslearning/learningtools/myfirstlearningtool/web/BaseServlet.java
+++ MyFirstLearningTool/src/java/itslearning/learningtools/myfirstlearningtool/web/BaseServlet.java
@@ -15,18 +15,17 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class BaseServlet extends HttpServlet
 {
-
     /**
-     *   1. Checks if the request is not expired. If it is expired, SecurityAccessDeniedException is thrown.
-    2. Validates the signature. If it is not valid, SecurityAccessDeniedException is thrown.
-    3. Puts ApiSessionId into your application session state.
-    4. Puts ApiSession object into your application session state (it it useful for RestApi.Client .NET wrapper assembly).
-    5. Puts all other received parameters into your application session state
-    (UserId, FirstName, LastName, Locale, Language, Use12hTimeFormat,
-    Accessibility, Permissions,
-    WindowsTimeZoneId, OlsonTimeZoneId).
-    To validate timestamp and signature the method reads application key,
-    shared secret and request lifetime from a source implementing IApplicationSettings interface.
+     * 1. Checks if the request is not expired. If it is expired, SecurityAccessDeniedException is thrown.
+     * 2. Validates the signature. If it is not valid, SecurityAccessDeniedException is thrown.
+     * 3. Puts ApiSessionId into your application session state.
+     * 4. Puts ApiSession object into your application session state (it it useful for RestApi.Client .NET wrapper assembly).
+     * 5. Puts all other received parameters into your application session state
+     * (UserId, FirstName, LastName, Locale, Language, Use12hTimeFormat,
+     * Accessibility, Permissions,
+     * WindowsTimeZoneId, OlsonTimeZoneId).
+     * To validate timestamp and signature the method reads application key,
+     * shared secret and request lifetime from a source implementing IApplicationSettings interface.
      * @param request
      */
     protected void createSession(HttpServletRequest request)
