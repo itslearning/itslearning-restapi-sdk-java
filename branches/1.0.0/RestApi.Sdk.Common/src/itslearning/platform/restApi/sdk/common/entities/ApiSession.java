78c1c4376da246230b1f4833c16487d1ebf7cd24
amundoz <amundoz@513eb65a-5214-11de-a3da-17e148e7a168>
2009-09-14 09:28:30 +0000

    Version 1.0.0 branching

---
+++ branches/1.0.0/RestApi.Sdk.Common/src/itslearning/platform/restApi/sdk/common/entities/ApiSession.java
@@ -0,0 +1,134 @@
+package itslearning.platform.restApi.sdk.common.entities;
+
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Class represents API session.
+ * @author Amund Trovåg <amund@itslearning.com>
+ */
+public class ApiSession implements Serializable
+{
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+
+    static
+    {
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
+    
+    private String _applicationKey;
+    private String _hash;
+    private String _sessionId;
+    private Date _lastRequestDateTimeUtc;
+
+    public ApiSession()
+    {
+    }
+
+    public ApiSession(String sessionId)
+    {
+        _sessionId = sessionId;
+        // TODO: is it correct to set it here? It's set here to prevent getTimeStamp() to fail in some cases where this is null
+        _lastRequestDateTimeUtc = new Date();
+    }
+
+    public String getTimeStamp()
+    {
+        return sdf.format(getLastRequestDateTimeUtc());
+    }
+
+    public static Date parseTimeStamp(String str)
+    {
+        try
+        {
+            return sdf.parse(str);
+        } catch (ParseException ex)
+        {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * Creates a new instance of the class with specified Id.
+     * @param sessionId
+     * @return New instance
+     */
+    public static ApiSession createApiSession(String sessionId)
+    {
+        if (sessionId == null || sessionId.length()<1)
+        {
+            return null;
+        }
+
+        return new ApiSession(sessionId);
+    }
+
+    /**
+     * @return the _applicationKey
+     */
+    public String getApplicationKey()
+    {
+        return _applicationKey;
+    }
+
+    /**
+     * @param applicationKey the _applicationKey to set
+     */
+    public void setApplicationKey(String applicationKey)
+    {
+        this._applicationKey = applicationKey;
+    }
+
+    /**
+     * @return the _hash
+     */
+    public String getHash()
+    {
+        return _hash;
+    }
+
+    /**
+     * @param hash the _hash to set
+     */
+    public void setHash(String hash)
+    {
+        this._hash = hash;
+    }
+
+    /**
+     * @return the _sessionId
+     */
+    public String getSessionId()
+    {
+        return _sessionId;
+    }
+
+    /**
+     * @param sessionId the _sessionId to set
+     */
+    public void setSessionId(String sessionId)
+    {
+        this._sessionId = sessionId;
+    }
+
+    /**
+     * @return the _lastRequestDateTimeUtc
+     */
+    public Date getLastRequestDateTimeUtc()
+    {
+        return _lastRequestDateTimeUtc;
+    }
+
+    /**
+     * @param lastRequestDateTimeUtc the _lastRequestDateTimeUtc to set
+     */
+    public void setLastRequestDateTimeUtc(Date lastRequestDateTimeUtc)
+    {
+        this._lastRequestDateTimeUtc = lastRequestDateTimeUtc;
+    }
+}
