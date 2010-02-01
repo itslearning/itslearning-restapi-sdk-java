b304ae864ca29d0c68f294f5bc4c631835be3a99
kristian.schmidt@itslearning.com <kristian.schmidt@itslearning.com@513eb65a-5214-11de-a3da-17e148e7a168>
2010-02-01 14:52:29 +0000

    Branch for Common project

---
+++ branches/1.0.2/RestApi.Sdk.Common/src/itslearning/platform/restApi/sdk/common/exceptions/ItslExceptionCode.java
@@ -0,0 +1,17 @@
+
+package itslearning.platform.restApi.sdk.common.exceptions;
+
+/**
+ * Error codes for business logic exceptions.
+ * @author Amund
+ */
+public enum ItslExceptionCode
+{
+        Unknown,
+        ObjectNotFound,
+        UniqueViolation,
+        LearningToolNotAllowComments,
+        LearningToolNotAllowSettingAssesment,
+        InvalidOperation,
+        VersionDoesNotExist
+}
