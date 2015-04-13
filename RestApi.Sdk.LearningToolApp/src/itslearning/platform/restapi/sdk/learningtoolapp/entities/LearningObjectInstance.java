e1db8413b5316dc15fd17898fd64d33447dba6c6
oleg.tarusov@gmail.com <oleg.tarusov@gmail.com@513eb65a-5214-11de-a3da-17e148e7a168>
2015-04-13 08:19:02 +0000

    [ITS-18313] Update API doc + java SDK. ParentId: [ITS-17871]. CheckedBy: none

--- RestApi.Sdk.LearningToolApp/src/itslearning/platform/restapi/sdk/learningtoolapp/entities/LearningObjectInstance.java
+++ RestApi.Sdk.LearningToolApp/src/itslearning/platform/restapi/sdk/learningtoolapp/entities/LearningObjectInstance.java
@@ -27,6 +27,7 @@ public class LearningObjectInstance {
     private int courseOrganisationId;
     private String courseOrganisationSyncKey;
     private Double maxScore;
+    private boolean useAnonymousSubmission;
     /**
      * @return the learningObjectInstanceId
      */
@@ -343,5 +344,21 @@ public class LearningObjectInstance {
     public Boolean getUseScore()
     {
         return getAssessmentId() != null ? getAssessmentId() == Constants.ScoreAssessmentId : null;
+    }
+    
+    /**
+     * @return the useAnonymousSubmission
+     */
+    public Boolean getUseAnonymousSubmission()
+    {
+        return useAnonymousSubmission;
+    }
+
+    /**
+     * @param useAnonymousSubmission Indicates that teachers should not see students names
+     */
+    public void setUseAnonymousSubmission(Boolean useAnonymousSubmission)
+    {
+        this.useAnonymousSubmission = useAnonymousSubmission;
     }
 }
